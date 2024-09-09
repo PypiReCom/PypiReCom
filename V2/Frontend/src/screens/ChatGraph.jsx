f751d34ccf5042e9ce754d41309a49d0ef36813c
Ivavi17 <avi17803@gmail.com>
2024-09-09 23:03:08 +0530

    added graph to chat with graph page

--- V2/Frontend/src/screens/ChatGraph.jsx
+++ V2/Frontend/src/screens/ChatGraph.jsx
@@ -1,14 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import GraphComponent from "../components/Graph";
 import { BASE_URL } from "../api-endpoint";
 
 const ChatGraph = () => {
   const { searchText } = useParams();  // Fetch searchText from URL parameters
   const [userMessage, setUserMessage] = useState('');
   const [chatMessages, setChatMessages] = useState([]);
+  const [graphData, setGraphData] = useState(null); // State to store fetched graph data
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null); // State to handle errors
+  const [loading, setLoading] = useState(true); // State to handle loading
+
+
+  const fetchData = async () => {
+    try {
+      setLoading(true); // Start loading
+      const response = await fetch(`${BASE_URL}/search?Search_Text=${searchText}`);
+      const data = await response.json();
+      
+      if (data && data.result) {
+        setGraphData(data); // Set the graph data if successful
+      } else {
+        setError('No graph data found');
+      }
+    } catch (err) {
+      setError('Error fetching graph data');
+    } finally {
+      setLoading(false); // End loading
+    }
+  };
+
+  useEffect(() => {
+    if (searchText) {
+      fetchData();
+    }
+  }, [searchText]);
+
+
+
+
+  
 
   // Function to handle sending a message
   const handleSendMessage = async () => {
@@ -54,6 +88,9 @@ const ChatGraph = () => {
       <Navbar />
       <div style={{ maxWidth: '900px', margin: '0 auto', padding: '20px' }}>
         <h3 style={{ textAlign: 'center', marginBottom: '20px' }}>Chat with  {searchText} Graph</h3>
+
+        {graphData && <GraphComponent data={graphData} />} {/* Render GraphComponent with fetched data */}
+
         {/* <p style={{ textAlign: 'center', color: '#666' }}>Search Text: {searchText}</p> */}
 
         {/* Chatbox container */}
@@ -81,6 +118,7 @@ const ChatGraph = () => {
               <pre>{chat.message}</pre>
             </div>
           ))}
+                  
           {isLoading && <p style={{ textAlign: 'center', color: '#666' }}>Loading...</p>}
         </div>
 
